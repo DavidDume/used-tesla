720aac0d9626289851f755229caaf4156c7c37f4
David Dume <dumedavid11@gmail.com>
2023-11-05 14:48:58 +0100

    modified user model

--- server/models/User.model.ts
+++ server/models/User.model.ts
@@ -1,13 +1,35 @@
 import mongoose from 'mongoose'
 
+export interface UserDocument extends Document {
+    email: string,
+    username: string,
+    password: string
+}
+
 const schema: mongoose.Schema = new mongoose.Schema(
     {
-        name: {
+        email: {
             type: String,
-            required: true
-        }
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: true,
+            length: [8, 'Password must be at least 8 characters long']
+
+        },
     },
     { timestamps: true }
 )
 
-export default mongoose.model("User", schema)
+export default mongoose.model<UserDocument>("User", schema)
