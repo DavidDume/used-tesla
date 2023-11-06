7574608e2b2ef2e8f6407ae8a271857893c452b1
David Dume <dumedavid11@gmail.com>
2023-11-06 16:37:58 +0100

    show users listings

--- pages/account.vue
+++ pages/account.vue
@@ -2,9 +2,14 @@
     <MainLayout>
         <div class="text-center">
             <h1>Ben tornato {{ userStore.user.username }}</h1>
-            <RouterLink to="/add-car">Vendi la tua tesla</RouterLink>
+            <RouterLink to="/add-car" class="inline-block border rounded-lg p-2 mt-2">Vendi la tua tesla</RouterLink>
         </div>
-        
+        <div class="m-3">
+            <h1>I tuoi annunci</h1>
+            <div v-if="usersListings" v-for="(listing, i) in usersListings.data" :key="i">
+                {{ listing.name }}
+            </div>
+        </div>
     </MainLayout>
 </template>
 
@@ -18,7 +23,12 @@
 
     const userStore = useUserStore()
 
+    let usersListings = ref()
+    onBeforeMount(async () => {
+        usersListings.value = await useFetch(`/api/cars/findByUser/${userStore.user._id}`)
+    })
 
+    
 </script>
 
 <style scoped>
