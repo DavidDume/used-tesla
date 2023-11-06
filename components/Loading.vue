20bb35d4c87475c5609f10b38c7e7b4ee11a0696
David Dume <dumedavid11@gmail.com>
2023-11-06 11:39:45 +0100

    loading component

---
+++ components/Loading.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex items-center justify-center">
+        <Icon name="eos-icons:loading" size="100" />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
