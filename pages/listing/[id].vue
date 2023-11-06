4474b266acda813d08032c01b21c45251fb6f3d3
David Dume <dumedavid11@gmail.com>
2023-11-06 15:42:43 +0100

    page for single listing

---
+++ pages/listing/[id].vue
@@ -0,0 +1,18 @@
+<template>
+   <MainLayout>
+        {{ listing }}
+   </MainLayout>
+</template>
+
+<script setup lang="ts">
+    import MainLayout from '../../layouts/MainLayout.vue';
+    const route = useRoute()
+    let listing = ref()
+    onBeforeMount(async () => {
+        listing.value = await $fetch(`/api/cars/${route.params.id}`)
+    })
+</script>
+
+<style scoped>
+
+</style>
