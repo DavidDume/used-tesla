557dd95e3248caf49f3d5a636f05841112f38086
David Dume <dumedavid11@gmail.com>
2023-11-09 18:29:09 +0100

    return car

--- server/api/cars/create.post.ts
+++ server/api/cars/create.post.ts
@@ -3,13 +3,14 @@ import { CarSchema } from "~/server/validations";
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
-    console.log(body);
 
     const { error } = CarSchema.validate(body)
     if (error) {
         throw error
 
     }
-    await CarModel.create(body)
-    return { message: 'Car created' }
+
+    const car = await CarModel.create(body)
+
+    return car
 })
