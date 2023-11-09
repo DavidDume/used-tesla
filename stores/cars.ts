c6a8d8de95b9105a516cf0167ad97ce3b37649d0
David Dume <dumedavid11@gmail.com>
2023-11-09 18:28:55 +0100

    type error solved

--- stores/cars.ts
+++ stores/cars.ts
@@ -3,24 +3,33 @@ import { ICar } from '~/types'
 
 export const useListingsStore = defineStore('listings', {
     state: () => ({
-        cars: {} as ICar
+        cars: [] as ICar[]
     }),
     actions: {
         async getAllCars() {
-            this.cars = await useFetch<ICar>('/api/cars/')
+            const { data, error } = await useFetch<ICar[]>('/api/cars/')
+
+            if (error.value) {
+                // Handle error (for example, you could set an error state in your store)
+                console.error('Failed to fetch cars:', error.value)
+                return
+            }
+
+            if (data.value) {
+                // Assign the data to `this.cars`
+                this.cars = data.value
+            }
         },
         async addCar(data: object) {
             try {
-                await $fetch('/api/cars/create', {
+                const car = await $fetch('/api/cars/create', {
                     method: 'POST',
                     body: data
                 })
-                return { message: 'Success' }
+                return car
             } catch (e) {
                 throw e
             }
-
         }
     }
-
 })
